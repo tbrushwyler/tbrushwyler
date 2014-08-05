c40ccb7f34515eed99560c54915083f9b6cc9171
Taylor Brushwyler <tbrushwyler@ostusa.com>
2014-08-04 21:39:08 -0400

    Add cumulo.ninja.landing

---
+++ cumulo.ninja.landing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cumulo.ninja.landing.MvcApplication" Language="C#" %>
---
+++ cumulo.ninja.landing/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cumulo.ninja.landing.Startup))]
+namespace cumulo.ninja.landing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
