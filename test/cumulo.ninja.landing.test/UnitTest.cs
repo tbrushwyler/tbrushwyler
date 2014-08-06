a60a9ccd7f48cc1a4b973668d53460eb6846b006
Taylor Brushwyler <tbrushwyler@ostusa.com>
2014-08-05 20:20:20 -0400

    Add tests for the landing site

---
+++ test/cumulo.ninja.landing.test/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace cumulo.ninja.landing.test
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void TrueTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
